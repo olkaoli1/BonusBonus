f8e29005d9c2d482f5cc6bd49c95c1c4de6f2653
Olga <oli.samsonowa@yandex.ru>
2024-11-11 01:51:14 +0300

    Добавлены дополнительные тесты и проведена настройка

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -8,15 +8,15 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        // Подготавливаем данные
+        // Подготавливаем данные:
         long amount = 1000;
         boolean registered = true;
-        long expected = 30;
+        long expected = 30; // для зарегистрированного пользователя бонус 3%
 
-        // Вызываем целевой метод
+        // Вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // Проверяем результат
+        // Производим проверку:
         Assertions.assertEquals(expected, actual);
     }
 
@@ -24,15 +24,48 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        // Подготавливаем данные
+        // Подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = true;
-        long expected = 500;
+        long expected = 500; // бонус ограничен лимитом 500
 
-        // Вызываем целевой метод
+        // Вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // Проверяем результат
+        // Производим проверку:
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // Подготавливаем данные:
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10; // для незарегистрированного пользователя бонус 1%
+
+        // Вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // Производим проверку:
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // Подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500; // бонус ограничен лимитом 500
+
+        // Вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // Производим проверку:
         Assertions.assertEquals(expected, actual);
     }
 }
+
